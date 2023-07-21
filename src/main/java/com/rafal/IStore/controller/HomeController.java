07264e2d68711401bbe99e0aace371e25625f8a4
Rafal <rafalwieczorek881@gmail.com>
2023-07-21 15:56:58 +0200

    base frontend, basic controller

---
+++ src/main/java/com/rafal/IStore/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.rafal.IStore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        // new Item("Jordan 1 Chicago", new BigDecimal(2100), "https://images.stockx.com/images/Air-Jordan-1-Retro-Chicago-2015-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1607656901");
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+}
---
+++ src/test/java/com/rafal/IStore/IStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafal.IStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
