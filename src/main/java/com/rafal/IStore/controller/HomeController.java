a1a61ca41feb2f191b0ca212291fa282abd50fb6
Rafal <rafalwieczorek881@gmail.com>
2023-07-24 23:52:55 +0200

    adding to cart, disp quantity and price

--- src/main/java/com/rafal/IStore/controller/HomeController.java
+++ src/main/java/com/rafal/IStore/controller/HomeController.java
@@ -1,23 +1,28 @@
 package com.rafal.IStore.controller;
 
+import com.rafal.IStore.Busket;
+import com.rafal.IStore.model.Item;
 import com.rafal.IStore.repository.ItemRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.HttpSessionRequiredException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class HomeController {
 
     private final ItemRepository itemRepository;
+    private final Busket busket;
 
     @Autowired
-    public HomeController(ItemRepository itemRepository) {
+    public HomeController(ItemRepository itemRepository, Busket busket) {
         this.itemRepository = itemRepository;
+        this.busket = busket;
     }
-
 
     @GetMapping("/")
     public String home(Model model, HttpSession httpSession){
@@ -25,6 +30,17 @@ public class HomeController {
         return "home";
     }
 
+    @GetMapping("/add/{itemId}")
+    public String addItemToBasket(@PathVariable("itemId") Long itemId, Model model){
+
+        Optional<Item> oItem = itemRepository.findById(itemId);
+        if (oItem.isPresent()){
+            Item item = oItem.get();
+            busket.addItem(item);
+        }
+        model.addAttribute("items" ,itemRepository.findAll());
+        return "home";
+    }
     @GetMapping("/login")
     public String login(){
         return "login";
