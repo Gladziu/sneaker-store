c61949c5e63cf79b5f38e6d2c8744b095a802aa4
Rafal <rafalwieczorek881@gmail.com>
2023-07-24 20:45:21 +0200

    added DB

--- src/main/java/com/rafal/IStore/controller/HomeController.java
+++ src/main/java/com/rafal/IStore/controller/HomeController.java
@@ -1,14 +1,27 @@
 package com.rafal.IStore.controller;
 
+import com.rafal.IStore.repository.ItemRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.HttpSessionRequiredException;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private final ItemRepository itemRepository;
+
+    @Autowired
+    public HomeController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+
     @GetMapping("/")
-    public String home(){
-        // new Item("Jordan 1 Chicago", new BigDecimal(2100), "https://images.stockx.com/images/Air-Jordan-1-Retro-Chicago-2015-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1607656901");
+    public String home(Model model, HttpSession httpSession){
+        model.addAttribute("items" ,itemRepository.findAll());
         return "home";
     }
 
--- src/main/java/com/rafal/IStore/model/Item.java
+++ src/main/java/com/rafal/IStore/model/Item.java
@@ -1,19 +1,32 @@
 package com.rafal.IStore.model;
 
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 import java.math.BigDecimal;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
+@Entity
 public class Item {
 
+    @Id
+    @GeneratedValue
+    private Long id;
     private String name;
     private BigDecimal price;
     private String urlImage;
+
+    public Item(String name, BigDecimal price, String urlImage) {
+        this.name = name;
+        this.price = price;
+        this.urlImage = urlImage;
+    }
 }
