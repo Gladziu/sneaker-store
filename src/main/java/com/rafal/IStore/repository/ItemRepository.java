9a91ef7bffb1d7c37ca946c2a643ec1affb71717
Rafal <rafalwieczorek881@gmail.com>
2023-07-24 23:54:06 +0200

    item repository

---
+++ src/main/java/com/rafal/IStore/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.rafal.IStore.repository;
+
+import com.rafal.IStore.model.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+}
