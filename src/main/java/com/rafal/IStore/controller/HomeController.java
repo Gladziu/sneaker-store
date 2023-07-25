bffcf683b1b4656b0075f4d5d520dd471cd2c67a
Rafal <rafalwieczorek881@gmail.com>
2023-07-25 13:41:38 +0200

    included busket service

--- src/main/java/com/rafal/IStore/controller/HomeController.java
+++ src/main/java/com/rafal/IStore/controller/HomeController.java
@@ -1,8 +1,6 @@
 package com.rafal.IStore.controller;
 
-import com.rafal.IStore.Busket;
-import com.rafal.IStore.model.Item;
-import com.rafal.IStore.repository.ItemRepository;
+import com.rafal.IStore.service.BusketService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,37 +8,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.Optional;
-
 @Controller
 public class HomeController {
 
-    private final ItemRepository itemRepository;
-    private final Busket busket;
+    private final BusketService busketService;
 
     @Autowired
-    public HomeController(ItemRepository itemRepository, Busket busket) {
-        this.itemRepository = itemRepository;
-        this.busket = busket;
+    public HomeController(BusketService busketService) {
+        this.busketService = busketService;
     }
 
     @GetMapping("/")
     public String home(Model model, HttpSession httpSession){
-        model.addAttribute("items" ,itemRepository.findAll());
+        model.addAttribute("items" ,busketService.getAllItems());
         return "home";
     }
 
     @GetMapping("/add/{itemId}")
     public String addItemToBasket(@PathVariable("itemId") Long itemId, Model model){
-
-        Optional<Item> oItem = itemRepository.findById(itemId);
-        if (oItem.isPresent()){
-            Item item = oItem.get();
-            busket.addItem(item);
-        }
-        model.addAttribute("items" ,itemRepository.findAll());
+        busketService.addItemToBusket(itemId);
+        model.addAttribute("items" ,busketService.getAllItems());
         return "home";
     }
+
     @GetMapping("/login")
     public String login(){
         return "login";
