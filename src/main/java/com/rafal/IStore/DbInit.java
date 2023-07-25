93b6b4f949d96704e11da2bbf6d13885a266cf3f
Rafal <rafalwieczorek881@gmail.com>
2023-07-25 13:40:35 +0200

    added diff sneakers

--- src/main/java/com/rafal/IStore/DbInit.java
+++ src/main/java/com/rafal/IStore/DbInit.java
@@ -25,8 +25,8 @@ public class DbInit implements CommandLineRunner {
     public void run(String... args) throws Exception {
         itemRepository.saveAll(List.of(
          new Item("Jordan 1 Chicago", new BigDecimal(2100), "https://images.stockx.com/images/Air-Jordan-1-Retro-Chicago-2015-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1607656901"),
-         new Item("Jordan 1", new BigDecimal(2100), "https://images.stockx.com/images/Air-Jordan-1-Retro-Chicago-2015-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1607656901"),
-         new Item("Jordan", new BigDecimal(2100), "https://images.stockx.com/images/Air-Jordan-1-Retro-Chicago-2015-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1607656901")
+         new Item("LV Trainer", new BigDecimal(1750), "https://images.stockx.com/images/Louis-Vuitton-Trainer-Monogram-Demin-Product.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1655385933"),
+         new Item("Dunk Panda", new BigDecimal(550), "https://images.stockx.com/360/Nike-Dunk-Low-White-Black-2021-W/Images/Nike-Dunk-Low-White-Black-2021-W/Lv2/img01.jpg?fm=jpg&auto=compress&w=480&dpr=2&updated_at=1635341330&h=320&q=75")
         ));
     }
 }
