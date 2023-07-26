180324baac2c7e5e723d81f5ed911e28fdacb112
Rafal <rafalwieczorek881@gmail.com>
2023-07-26 02:36:50 +0200

    added show orders

--- src/main/java/com/rafal/IStore/controller/AdminController.java
+++ src/main/java/com/rafal/IStore/controller/AdminController.java
@@ -1,22 +1,29 @@
 package com.rafal.IStore.controller;
 
 import com.rafal.IStore.model.Item;
+import com.rafal.IStore.model.order.Order;
 import com.rafal.IStore.repository.ItemRepository;
+import com.rafal.IStore.repository.order.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
     private final ItemRepository itemRepository;
+    private final OrderRepository orderRepository;
 
     @Autowired
-    public AdminController(ItemRepository itemRepository) {
+    public AdminController(ItemRepository itemRepository, OrderRepository orderRepository) {
         this.itemRepository = itemRepository;
+        this.orderRepository = orderRepository;
     }
 
     @GetMapping
@@ -28,5 +35,11 @@ public class AdminController {
     private String addItem(Item item){
         itemRepository.save(item);
         return "redirect:/";
+    }
+
+    @GetMapping("/showorders")
+    @ResponseBody
+    public List<Order> showOrders(){
+        return orderRepository.findAll();
     }
 }
