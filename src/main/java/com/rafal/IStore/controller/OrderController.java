486d8d916e138f47955b288ff0bad75af5166402
Rafal <rafalwieczorek881@gmail.com>
2023-07-26 02:42:35 +0200

    added remove all items, summary, saveorder

--- src/main/java/com/rafal/IStore/controller/OrderController.java
+++ src/main/java/com/rafal/IStore/controller/OrderController.java
@@ -1,6 +1,9 @@
 package com.rafal.IStore.controller;
 
+import com.rafal.IStore.ItemOperation;
+import com.rafal.IStore.dto.OrderDto;
 import com.rafal.IStore.service.BusketService;
+import com.rafal.IStore.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,12 +17,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class OrderController {
 
     private final BusketService busketService;
+    private final OrderService orderService;
 
     @Autowired
-    public OrderController(BusketService busketService) {
+    public OrderController(BusketService busketService, OrderService orderService) {
         this.busketService = busketService;
+        this.orderService = orderService;
     }
-
 
     @GetMapping("/busket")
     public String showBusket(){
@@ -28,13 +32,32 @@ public class OrderController {
 
     @GetMapping("/increase/{itemId}")
     public String increaseItem(@PathVariable("itemId") Long itemId){
-        busketService.addItemToBusket(itemId);
+        busketService.itemOperation(itemId, ItemOperation.INCREASE);
         return "busket";
     }
 
     @GetMapping("/decrease/{itemId}")
     public String decreaseItem(@PathVariable("itemId") Long itemId){
-        busketService.removeItemFromBusket(itemId);
+        busketService.itemOperation(itemId, ItemOperation.DECREASE);
         return "busket";
     }
+
+    @GetMapping("/remove/{itemId}")
+    public String removeItemFromBasket(@PathVariable("itemId") Long itemId){
+        busketService.itemOperation(itemId, ItemOperation.REMOVE);
+        return "busket";
+    }
+
+    @GetMapping("/summary")
+    public String showSummary(){
+        return "summary";
+    }
+
+    @PostMapping("/saveorder")
+    public String saveOrder(OrderDto orderDto) {
+        orderService.saveOrder(orderDto);
+        return "redirect:/";
+    }
+
+
 }
