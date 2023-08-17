8dfdc21398fe33dc55199876420b46d1739fd2a1
Rafal <rafalwieczorek881@gmail.com>
2023-08-17 02:27:06 +0200

    validation tu checkout details

--- src/main/java/com/rafal/IStore/dto/OrderDto.java
+++ src/main/java/com/rafal/IStore/dto/OrderDto.java
@@ -1,5 +1,7 @@
 package com.rafal.IStore.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,9 +10,25 @@ import lombok.Setter;
 @Getter
 @Setter
 public class OrderDto {
+
+    @NotEmpty(message = "First name is required")
+    @Pattern(regexp = "^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+$", message = "Invalid first name")
     private String firstName;
+
+    @NotEmpty(message = "Last name is required")
+    @Pattern(regexp = "^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ ]+$", message = "Invalid last name")
     private String lastName;
+
+    @NotEmpty(message = "Address is required")
+    @Pattern(regexp = "^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ\\s]+\\s+[0-9]+(?:/[0-9]+)?|" +
+                      "^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ\\s]+\\s*[0-9]*$", message = "Invalid address")
     private String address;
+
+    @NotEmpty(message = "Postal code is required")
+    @Pattern(regexp = "^[0-9]{2}-[0-9]{3}$", message = "Invalid postal code")
     private String postCode;
+
+    @NotEmpty(message = "City is required")
+    @Pattern(regexp = "^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ ]+$", message = "Invalid city")
     private String city;
 }
