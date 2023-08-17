3393f4d47a18dc4f5f25730df3cf803d239c0cd8
Rafal <rafalwieczorek881@gmail.com>
2023-08-17 02:25:12 +0200

    added GeneratedValue strategy

--- src/main/java/com/rafal/IStore/model/order/Order.java
+++ src/main/java/com/rafal/IStore/model/order/Order.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class Order {
 
     @Id
-    @GeneratedValue()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
     private String firstName;
     private String lastName;
