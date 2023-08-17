1476bf2374bda00a8d182dc515c96ce70b89e365
Rafal <rafalwieczorek881@gmail.com>
2023-08-17 02:26:49 +0200

    check if the basket is not empty and added validation tu checkout details

--- src/main/java/com/rafal/IStore/controller/OrderController.java
+++ src/main/java/com/rafal/IStore/controller/OrderController.java
@@ -1,11 +1,17 @@
 package com.rafal.IStore.controller;
 
-import com.rafal.IStore.model.item.ItemOperation;
 import com.rafal.IStore.dto.OrderDto;
+import com.rafal.IStore.model.item.ItemOperation;
+import com.rafal.IStore.model.user.User;
 import com.rafal.IStore.service.busket.BusketService;
 import com.rafal.IStore.service.order.OrderService;
+import com.rafal.IStore.service.user.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,11 +23,13 @@ public class OrderController {
 
     private final BusketService busketService;
     private final OrderService orderService;
+    private final UserService userService;
 
     @Autowired
-    public OrderController(BusketService busketService, OrderService orderService) {
+    public OrderController(BusketService busketService, OrderService orderService, UserService userService) {
         this.busketService = busketService;
         this.orderService = orderService;
+        this.userService = userService;
     }
 
     @GetMapping("/busket")
@@ -48,12 +56,25 @@ public class OrderController {
     }
 
     @GetMapping("/summary")
-    public String showSummary(){
+    public String showSummary(Model model, Authentication authentication){
+        if(busketService.isBusketEmpty()){
+            model.addAttribute("busketIsEmpty", "Busket is empty!");
+            return "busket";
+        }
+        User user = userService.getCurrentUser(authentication);
+        String[] userSplited = user.getName().split(" ");
+        model.addAttribute("name", userSplited[0]);
+        model.addAttribute("surname", userSplited[1]);
+        model.addAttribute("orderDto", new OrderDto());
         return "summary";
     }
 
     @PostMapping("/save-order")
-    public String saveOrder(OrderDto orderDto) {
+    public String saveOrder(@Valid OrderDto orderDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "summary"; // Powrót do widoku z formularzem i wyświetleniem błędów
+        }
+
         orderService.saveOrder(orderDto);
         return "redirect:/sneaker-store/home";
     }
