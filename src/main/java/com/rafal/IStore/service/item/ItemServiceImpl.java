3e86e60c3fc900e8f832f4e644faaa94cc8cd251
Rafal <rafalwieczorek881@gmail.com>
2023-11-29 02:14:27 +0100

    add item

--- src/main/java/com/rafal/IStore/service/item/ItemServiceImpl.java
+++ src/main/java/com/rafal/IStore/service/item/ItemServiceImpl.java
@@ -1,38 +1,40 @@
 package com.rafal.IStore.service.item;
 
 import com.rafal.IStore.model.item.Item;
-import com.rafal.IStore.model.item.ItemOperation;
 import com.rafal.IStore.repository.ItemRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.Optional;
 
 @Service
-@AllArgsConstructor
 public class ItemServiceImpl implements ItemService {
 
-    @Autowired
-    private ItemRepository itemRepository;
+    private final ItemRepository itemRepository;
+
+    public ItemServiceImpl(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
 
     @Override
-    public void deleteItem(Long itemId) { itemRepository.deleteById(itemId); }
+    public void deleteItem(Long itemId) {
+        itemRepository.deleteById(itemId);
+    }
 
     @Override
-    public void editItem(Long itemId, BigDecimal newPrice, String newName, String newUrlImg) {
-        Optional<Item> oItem = itemRepository.findById(itemId);
-        if (oItem.isPresent()) {
-            Item item = oItem.get();
-            item.setPrice(newPrice);
-            item.setName(newName);
-            item.setUrlImage(newUrlImg);
+    public void editItem(Item item) {
+        Optional<Item> existingOptionalItem = itemRepository.findById(item.getId());
+        if (existingOptionalItem.isPresent()) {
             itemRepository.save(item);
         }
     }
 
     @Override
-    public Item findItemById(Long itemId) { return itemRepository.findById(itemId).orElse(null);
+    public Item findItemById(Long itemId) {
+        return itemRepository.findById(itemId).orElse(null);
+    }
+
+    @Override
+    public void addItem(Item item) {
+        itemRepository.save(item);
     }
 }
