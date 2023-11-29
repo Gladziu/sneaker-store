775d31b18678221dab9c7156187d6ae5fdd9e6f5
Rafal <rafalwieczorek881@gmail.com>
2023-11-29 02:15:24 +0100

    orders history

--- src/main/java/com/rafal/IStore/service/order/OrderServiceImpl.java
+++ src/main/java/com/rafal/IStore/service/order/OrderServiceImpl.java
@@ -3,25 +3,43 @@ package com.rafal.IStore.service.order;
 import com.rafal.IStore.dto.OrderDto;
 import com.rafal.IStore.mapper.OrderMapper;
 import com.rafal.IStore.model.order.Order;
+import com.rafal.IStore.model.user.User;
 import com.rafal.IStore.repository.order.OrderItemRepository;
 import com.rafal.IStore.repository.order.OrderRepository;
 import com.rafal.IStore.service.basket.Basket;
-import lombok.RequiredArgsConstructor;
+import com.rafal.IStore.service.user.UserService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-@RequiredArgsConstructor
-public class OrderServiceImpl implements OrderService{
+public class OrderServiceImpl implements OrderService {
     private final Basket basket;
     private final OrderRepository orderRepository;
     private final OrderItemRepository orderItemRepository;
+    private final UserService userService;
+
+    public OrderServiceImpl(Basket basket, OrderRepository orderRepository, OrderItemRepository orderItemRepository, UserService userService) {
+        this.basket = basket;
+        this.orderRepository = orderRepository;
+        this.orderItemRepository = orderItemRepository;
+        this.userService = userService;
+    }
 
 
     @Override
-    public void saveOrder(OrderDto orderDto, String email) {
-        Order order = OrderMapper.mapToOrder(orderDto, basket, email);
+    public void saveOrder(OrderDto orderDto, Authentication authentication) {
+        User user = userService.getCurrentUser(authentication);
+        Order order = OrderMapper.mapToOrder(orderDto, basket, user.getEmail());
         orderRepository.save(order);
         orderItemRepository.saveAll(OrderMapper.mapToOrderItemList(basket, order));
         basket.clearBasket();
+    }
+
+    @Override
+    public List<Order> ordersHistory(Authentication authentication) {
+        User user = userService.getCurrentUser(authentication);
+        return orderRepository.findByEmail(user.getEmail());
     }
 }
