5c944c409df4f9ff96a259a69490be703ec42cbd
Rafal <rafalwieczorek881@gmail.com>
2023-12-14 20:11:00 +0100

    basket connection

--- src/main/java/com/rafal/IStore/model/user/User.java
+++ src/main/java/com/rafal/IStore/model/user/User.java
@@ -1,11 +1,12 @@
 package com.rafal.IStore.model.user;
 
+import com.rafal.IStore.model.basket.BasketInfo;
+import com.rafal.IStore.model.basket.BasketItem;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,21 +16,27 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String firstName;
+    private String lastName;
     @Column(unique = true)
     private String email;
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(
-            name = "users_roles",
+            name = "user_role",
             joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
             inverseJoinColumns = {@JoinColumn(name = "ROLE_ID", referencedColumnName = "ID")})
-    private List<Role> roles = new ArrayList<>();
+    private List<Role> role = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<BasketItem> basketItem = new ArrayList<>();
+
+    @OneToOne(mappedBy = "user")
+    private BasketInfo basketInfo;
 
 }
