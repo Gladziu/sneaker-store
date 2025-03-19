24aa1d71bbe403b50205cc382d29b2fff9a2bcb6
Gladziu <rafalwieczorek881@gmail.com>
2025-03-19 22:06:12 +0100

    added link for video to readme and small code refactor

--- src/main/java/com/rafal/sneakerstoreapp/user/CustomUserDetailsService.java
+++ src/main/java/com/rafal/sneakerstoreapp/user/CustomUserDetailsService.java
@@ -23,11 +23,12 @@ class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email);
-
         if (user != null) {
-            return new org.springframework.security.core.userdetails.User(user.getEmail(),
+            return new org.springframework.security.core.userdetails.User(
+                    user.getEmail(),
                     user.getPassword(),
-                    mapRoleToAuthoritiesCollection(user.getRole().getName()));
+                    mapRoleToAuthoritiesCollection(user.getRole().getName())
+            );
         } else {
             throw new UsernameNotFoundException("Invalid username or password.");
         }
